12d89d3b257203e9b5530e579576e3ee9edeb0a1
Tristyn Kahrs <79658190+TristynKahrs@users.noreply.github.com>
2022-08-12 11:52:22 -0600

    Authentication with one user, I hope

--- auth-service/User.cs
+++ auth-service/User.cs
@@ -2,7 +2,8 @@ public class User
 {
     public string? Username { get; set; }
     public string? Email { get; set; }
-    public string? Password { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? PasswordSalt { get; set; }
 
     public override string ToString()
     {
--- auth-service/UserController.cs
+++ auth-service/UserController.cs
@@ -8,16 +8,93 @@ namespace Controllers
     public class UserController : ControllerBase
     {
         private readonly User _db;
+        private readonly IConfiguration _configuration;
+        public static User user = new User();
 
-        public UserController(ILogger<UserController> logger, User db)
+        public UserController(ILogger<UserController> logger, User db, IConfiguration configuration)
         {            
             _db = db;
+            _configuration = configuration;
         }
 
         [HttpGet("abc")]
         public ActionResult<string> abc()
         {
             return "hello from abc";
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                // new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
         }
 
         // [HttpPost("")]
