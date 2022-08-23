13ccc213ecf7291a49663fda170fc9a65917a1ef
Lisset <lissetsadian@hotmail.com>
2022-08-22 18:52:00 -0600

    better cehckout logic, bad login logic, styling

--- frontEnd/javascript/checkout.js
+++ frontEnd/javascript/checkout.js
@@ -3,11 +3,15 @@ var checkoutButton = document.getElementById('checkoutButton');
 
 checkoutButton.addEventListener('click', function(e){
   console.log('checkout.js');
-  const totalObject = {total: total};
+  if(cartItems.length > 0){
+    const totalObject = {total: total};
   let NewArrayWithTotal = cartItems.concat(totalObject);
   //let cartItems = cartItems.unshift(totalObject);
+  
   let myJSONString = JSON.stringify(NewArrayWithTotal);
   console.log(cartItems);
   console.log(myJSONString);
+  window.location.href = 'Checkout.html';
+  }
 });
 
--- frontEnd/javascript/login.js
+++ frontEnd/javascript/login.js
@@ -1,1 +1,46 @@
 console.log('login.js');
+
+var loginButton = document.getElementById('loginSubmit');
+
+async function sendLogintoBackend (email, password) {
+  const data = { username: 'example' };
+
+  fetch('https://example.com/profile', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  
+  const { token } = response.body
+
+  const customerInfo = {
+    "username": "exampelusername"
+  }
+  
+  localStorage.setItem('token', token)
+  if (isLoggedIn === true) {
+    
+    window.location.href = 'Catalog.html'
+  }
+  else{
+    alert('Incorrect email or password')
+  }
+
+}
+
+async function isLoggedIn () {
+  const token = store.get('token')
+  if (!token) return false
+}
+
+
+loginButton.addEventListener('click', basiclogin);
