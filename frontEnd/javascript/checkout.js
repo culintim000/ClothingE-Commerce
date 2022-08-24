eb74a1babef200df6dc5058214e394dfff2e200f
Lissetx <lissetsadian@hotmail.com>
2022-08-24 08:23:16 -0600

    fixed cehckout, slight style change

--- frontEnd/javascript/checkout.js
+++ frontEnd/javascript/checkout.js
@@ -1,18 +1,20 @@
 var checkoutButton = document.getElementById('checkoutButton');
-const {customerInfo} = require('./login');
+//const {customerInfo} = require('./login');
 
 
 checkoutButton.addEventListener('click', function(e){
   console.log('checkout.js');
-  if(cartItems.length > 0){
+  
   const totalObject = {total: total};
-  let NewArrayWithTotal = cartItems.concat(customerInfo).concat(totalObject);
+  let NewArrayWithTotal = cartItems.concat(totalObject);
+
+  //.concat(customerInfo)
   //let cartItems = cartItems.unshift(totalObject);
   
   let myJSONString = JSON.stringify(NewArrayWithTotal);
   console.log(cartItems);
   console.log(myJSONString);
   window.location.href = 'Checkout.html';
-  }
+  
 });
 
